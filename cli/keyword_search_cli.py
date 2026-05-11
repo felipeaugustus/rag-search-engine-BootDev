33f2a96ada064d0c6788c3377a235360bfbf9543
Felipe Augusto de Carvalho de Araújo <103113217+felipeaugustus@users.noreply.github.com>
2026-05-11 19:03:31 -0300

    tf_idf

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,24 +2,59 @@
 
 import argparse
 
-from lib.keyword_search import search_command
+from lib.keyword_search import (
+    build_command,
+    idf_command,
+    search_command,
+    tf_command,
+    tf_idf_command
+)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    subparsers.add_parser("build", help="Build the inverted index")
+
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    tf_parser = subparsers.add_parser(
+        "tf", help="Get term frequency for a given document ID and term"
+    )
+    tf_parser.add_argument("doc_id", type=int, help="Document ID")
+    tf_parser.add_argument("term", type=str, help="Term to get frequency for")
+
+    idf_parser = subparsers.add_parser(
+        "idf", help="Get inverse document frequency for a given term"
+    )
+    idf_parser.add_argument("term", type=str, help="Term to get IDF for")
+
+    tfidf_parser = subparsers.add_parser("tfidf", help="Get TF-IDF score for a given term")
+    tfidf_parser.add_argument("doc_id", type=int, help="Document ID")
+    tfidf_parser.add_argument("term", type=str, help="Term to get TF-IDF for")
     args = parser.parse_args()
 
     match args.command:
+        case "build":
+            print("Building inverted index...")
+            build_command()
+            print("Inverted index built successfully.")
         case "search":
             print("Searching for:", args.query)
             results = search_command(args.query)
             for i, res in enumerate(results, 1):
-                print(f"{i}. {res['title']}")
+                print(f"{i}. ({res['id']}) {res['title']}")
+        case "tf":
+            tf = tf_command(args.doc_id, args.term)
+            print(f"Term frequency of '{args.term}' in document '{args.doc_id}': {tf}")
+        case "idf":
+            idf = idf_command(args.term)
+            print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
+        case "tfidf":
+            tf_idf = tf_idf_command(args.doc_id, args.term)
+            print(f"TF-IDF score of '{args.term}' in document '{args.doc_id}': {tf_idf:.2f}")
         case _:
             parser.print_help()
 
