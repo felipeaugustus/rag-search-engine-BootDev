d924be1d67b3aa12e670704fd2f230841d0728d6
Felipe Augusto de Carvalho de Araújo <103113217+felipeaugustus@users.noreply.github.com>
2026-06-07 12:25:23 -0300

    start mbeddings

---
+++ cli/semantic_search_cli.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from lib.semantic_search import verify_model, embed_text
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    subparsers.add_parser("verify", help="Verify the embedding model loads correctly")
+    eb = subparsers.add_parser("embed_text", help="Embed the text into a model")
+    eb.add_argument("text", type=str, help="Text to be embedded")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "verify":
+            verify_model()
+        case "embed_text":
+            embed_text(args.text)
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
